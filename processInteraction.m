30c60142af0c77651b61e694af6be77e6037cfe1
tchieze <allekmott@gmail.com>
2015-07-01 14:32:49 -0700

    Add more defined skeleton for processInteraction

--- processInteraction.m
+++ processInteraction.m
@@ -1,3 +1,17 @@
+interaction = 0
+
 function [state] = followLine(brick)
+	interaction = interaction + 1;
+	if interaction == 1
+		fprintf('Interaction 1\n');
+		% TODO code for interaction 1
+	else if interaction == 2
+		fprintf('Interaction 2\n');
+		% TODO code for interaction 2
+	else
+		fprintf('Interaction ..wait wtf?\n');
+	end
+
+	% TODO global state constants?
 	state = 0;
 end
