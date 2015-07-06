14a2d289d1488358d249fdf1dc4b1f6680ba0c4d
brosenlof <brosenlof@users.noreply.github.com>
2015-07-06 12:39:46 -0700

    Update followLine.m

--- followLine.m
+++ followLine.m
@@ -1,14 +1,14 @@
-function [state] = followLine()
-%will require brick object as input argument once implemented into race
+function [state] = followLine(brick)
+% Will require brick object as input argument once implemented into race
 
 %robot start processes will be removed and done only once at race start
-address = '00165311686b'
-brick = lego.NXT(address)
-brick.getBatteryLevel() /9000
+%address = '00165311686b'
+%brick = lego.NXT(address)
+%brick.getBatteryLevel() /9000
 
 lightSensor = lego.NXT.IN_1;
-leftWheel = lego.NXT.OUT_A;
-rightWheel = lego.NXT.OUT_C;
+%leftWheel = lego.NXT.OUT_A;
+%rightWheel = lego.NXT.OUT_C;
 %both wheels A and C together
 wheels = lego.NXT.OUT_AC;
 
@@ -18,26 +18,34 @@ if retVal == 0
     brick.resetInputScaledValue(lightSensor);
 end
 
-%Approx light ranges on desktop -- may be different in race box
-%all blue tape ~285-295
-%mix ~330 - 360
-%white ~450-460
+% Approx light ranges on desktop -- may be different in race box
+% all blue tape ~285-295
+% mix ~330 - 360
+% white ~450-460
 
-%system pause before loop start
-pause(5);
+% system pause before loop start
 
 tic % start timer
 steer = 0;
-while toc < 15 % run for 15 seconds
+while toc < 8 % run for 15 seconds %change to test for red line
     senseValue = brick.sensorValue(lightSensor);
-    
-    if senseValue > 380
+     steer = 0;
+    if senseValue > 350
         steer = steer + 1;
+    elseif senseValue < 335;
+        steer = steer - 1;
     else
         steer = 0;
     end
-
-    brick.motorReverseSync(wheels,50,steer);
+    
+    if steer > 100 
+        steer = 100;
+    elseif steer < -100
+        steer = -100;
+    end
+    
+    % Second argument is power, increase after testing
+    brick.motorReverseSync(wheels,30,steer); 
     
     fprintf('Steer: %2.1f\n', steer);
     %fprintf('light: %2.1f\n', senseValue);
