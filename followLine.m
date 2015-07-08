7c681c6d5c07ff2e8f5a6a2fcafffc3a33897e1f
brosenlof <brosenlof@users.noreply.github.com>
2015-07-08 14:21:24 -0700

    Snapshot 2:20 7/8/2015; modifications to steering, add pause to handle harsh blues

--- followLine.m
+++ followLine.m
@@ -5,34 +5,66 @@ lightSensor = lego.NXT.IN_1;
 %both wheels A and C together
 wheels = lego.NXT.OUT_AC;
 
+leftWheel = lego.NXT.OUT_A;
+rightWheel = lego.NXT.OUT_C;
+
 % Approx light ranges on desktop -- may be different in race box
 % all blue tape ~285-295
 % mix ~330-360
 % white ~450-460
 
-avgLight = 345;
+% Under mount
+% white - 480 -490
+% blue - 290 -315
+% mix 330 - 345
 
-MAX_POWER = 30;
-power = MAX_POWER;
+%kill motors
+brick.motorReverseSync(wheels, 0, 0);
 
-steer = 0;
-senseValue = brick.sensorValue(lightSensor);
+avgLight = 370; % 337
 
-% TODO handle red/interactions & end
+MAX_POWER = 40;
+MIN_POWER = 20;
+MAX_STEER = 100;
 
-steer = senseValue - avgLight;
-power = power - (steer / 10);
+while true
+    power = MAX_POWER;
+    senseValue = brick.sensorValue(lightSensor);
+    
+    % TODO handle red/interactions & end
 
-if steer > 100 
-    steer = 100;
-elseif steer < -100
-    steer = -100;
+    diff = (senseValue - avgLight) / 1.5
+    powerReduction = 10^(3 - (abs(diff) / 20));
+    power = MAX_POWER - powerReduction
+    if (power < MIN_POWER)
+        power = MIN_POWER;
+    end
+
+    if diff < -20 % left; over blue
+        fprintf('blue\n');
+        if (diff < -40) % realllly blue; turn HARD
+            brick.motorForward(leftWheel, 35);
+            brick.motorReverse(rightWheel, 35);
+            pause(.3);
+        else
+            steer = -((diff/3) ^ 2)
+            if (steer > MAX_STEER)
+                steer = MAX_STEER;
+            end
+            brick.motorReverseSync(wheels, power, steer);
+        end
+    elseif diff > 20 %right; over white
+        fprintf('white\n');
+        steer = diff / 1.5
+        
+        brick.motorReverseSync(wheels, power, steer);
+    else %midpoint; over edge
+        fprintf('edge\n');
+        brick.motorReverseSync(wheels, power, 0);
+    end
+
+    %fprintf('Steer: %2.1f\n', steer);
+    %fprintf('light: %2.1f\n', senseValue);
+
 end
-
-% Second argument is power, increase after testing
-brick.motorReverseSync(wheels, power, steer); 
-
-fprintf('Steer: %2.1f\n', steer);
-%fprintf('light: %2.1f\n', senseValue);
-
 state = States.FOLLOW_LINE
